b7acf0754d02fb3342d63da6c8c9ae2300e2ee2d
zhrb <itbin@foxmail.com>
2019-09-19 11:11:00 +0800

    Init repository.

---
+++ src/main/java/com/zhrb/testDemo/TestCollection.java
@@ -0,0 +1,15 @@
+package com.zhrb.testDemo;
+
+/**
+ * @ClassName TestCollection
+ * @Description TODO
+ * @Author Administrator
+ * @Date 2019/9/19 11:07
+ * @Version
+ */
+public class TestCollection {
+
+    public static void main(String[] args) {
+        
+    }
+}
